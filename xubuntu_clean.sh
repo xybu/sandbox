6097437f7108d4fc1fdea9b43d94cb5e6b0a6563
Xiangyu Bu <xybu92@live.com>
2015-02-10 13:23:02 -0800

    changed xubuntu_clean.sh

--- xubuntu_clean.sh
+++ xubuntu_clean.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt-get remove abiword gimp gnome-sudoku gnome-mines gmusicbrowser pidgin thunderbird transmission-gtk transmission-common xfce4-weather-plugin xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sis xubuntu-artwork xubuntu-community-wallpapers dictionaries-common yelp xchat
+sudo apt-get -y remove abiword gimp gnome-sudoku gnome-mines
+sudo apt-get -y remove gmusicbrowser pidgin thunderbird transmission-gtk transmission-common xfce4-weather-plugin 
+sudo apt-get -y remove xubuntu-community-wallpapers
+sudo apt-get -y remove xchat
+sudo apt-get -y remove xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sis
