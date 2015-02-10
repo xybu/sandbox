26b5b0aff535f21809920f310a8435940ff39447
Xiangyu Bu <xybu92@live.com>
2015-02-10 13:07:31 -0800

    Added script to remove Xubuntu packages I dont use

---
+++ xubuntu_clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get remove abiword gimp gnome-sudoku gnome-mines gmusicbrowser pidgin thunderbird transmission-gtk transmission-common xfce4-weather-plugin xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sis xubuntu-artwork xubuntu-community-wallpapers dictionaries-common yelp xchat
