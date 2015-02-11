bf0ecaaaea3bdd4422e11606ffe8ccba2ff502a7
Xiangyu Bu <xybu92@live.com>
2015-02-10 21:45:35 -0800

    more devel scripts

--- xubuntu_clean.sh
+++ xubuntu_clean.sh
@@ -3,5 +3,5 @@
 sudo apt-get -y remove abiword gimp gnome-sudoku gnome-mines
 sudo apt-get -y remove gmusicbrowser pidgin thunderbird transmission-gtk transmission-common xfce4-weather-plugin 
 sudo apt-get -y remove xubuntu-community-wallpapers
-sudo apt-get -y remove xchat
+sudo apt-get -y remove xchat simple-scan
 sudo apt-get -y remove xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sis
---
+++ xubuntu_my.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get -y install gnome-system-monitor
+# sudo apt-get -y install xubuntu-restricted-extras 
