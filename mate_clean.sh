91726a1f2618907d13803333c0ecb71a426aba6f
Xiangyu Bu <xybu92@live.com>
2015-02-22 07:42:48 -0500

    More on MATE clean

--- mate_clean.sh
+++ mate_clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y remove cheese hexchat libreoffice-core libreoffice-common libreoffice-base-core pidgin thunderbird ubuntu-mate-wallpapers-utopic vlc xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sis xserver-xorg-video-sisusb xserver-xorg-video-siliconmotion xserver-xorg-video-trident xserver-xorg-video-tdfx
-sudo apt-get -y remove transmission-gtk transmission-common
+sudo apt-get -y autoremove cheese hexchat libreoffice-core libreoffice-common libreoffice-base-core pidgin thunderbird vlc xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sis xserver-xorg-video-sisusb xserver-xorg-video-siliconmotion xserver-xorg-video-trident xserver-xorg-video-tdfx
+sudo apt-get -y autoremove transmission-gtk transmission-common
+sudo apt-get -y autoremove shotwell pidgin-otr brasero printer-driver-foo2zjs-common 
 
