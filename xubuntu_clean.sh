6e04a212310dfbfaae20017fde6772167ce60efa
Xiangyu Bu <xybu92@live.com>
2015-04-25 14:54:20 -0700

    Updated script for Vivid

--- xubuntu_clean.sh
+++ xubuntu_clean.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt-get -y remove abiword gimp gnome-sudoku gnome-mines
-sudo apt-get -y remove gmusicbrowser pidgin thunderbird transmission-gtk transmission-common xfce4-weather-plugin 
-sudo apt-get -y remove xubuntu-community-wallpapers
-sudo apt-get -y remove xchat simple-scan
-sudo apt-get -y remove xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sis
+sudo apt-get -y autoremove abiword abiword-common gimp gimp-data gimp-help-common gimp-help-en pidgin-data pidgin-otr gnome-sudoku gnome-mines
+sudo apt-get -y autoremove gmusicbrowser pidgin thunderbird transmission-gtk transmission-common xfce4-weather-plugin 
+sudo apt-get -y autoremove xubuntu-community-wallpapers
+sudo apt-get -y autoremove xchat simple-scan
+sudo apt-get -y autoremove xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sisusb
+sudo apt-get -y autoremove xserver-xorg-input-wacom xserver-xorg-video-ati xserver-xorg-video-trident xserver-xorg-video-r128
