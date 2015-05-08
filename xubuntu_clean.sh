8e96c6d364328a501f1bb7815b58e4712a63e5b5
Xiangyu Bu <xybu92@live.com>
2015-05-08 02:44:34 -0400

    Update xubuntu_clean.sh

--- xubuntu_clean.sh
+++ xubuntu_clean.sh
@@ -3,6 +3,6 @@
 sudo apt-get -y autoremove abiword abiword-common gimp gimp-data gimp-help-common gimp-help-en pidgin-data pidgin-otr gnome-sudoku gnome-mines
 sudo apt-get -y autoremove gmusicbrowser pidgin thunderbird transmission-gtk transmission-common xfce4-weather-plugin 
 sudo apt-get -y autoremove xubuntu-community-wallpapers
-sudo apt-get -y autoremove xchat simple-scan
+sudo apt-get -y autoremove xchat simple-scan xfburn
 sudo apt-get -y autoremove xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-sisusb
 sudo apt-get -y autoremove xserver-xorg-input-wacom xserver-xorg-video-ati xserver-xorg-video-trident xserver-xorg-video-r128
