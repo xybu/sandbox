86e6fa89aec7b699b45f6e145b26233256ce109c
Xiangyu Bu <xybu92@live.com>
2015-07-25 00:44:22 -0700

    Install Oracle JDK 8 by default

--- ubuntu_devel.sh
+++ ubuntu_devel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get -y install build-essential cmake automake gcc g++ gdb valgrind nemiver curl vim-gtk gawk ssh libcurl4-openssl-dev python-software-properties software-properties-common
+sudo apt-get -y install build-essential cmake automake gcc g++ gdb valgrind nemiver curl vim-gtk gawk ssh libcurl4-openssl-dev python-software-properties software-properties-common ack-grep
 sudo apt-get -y install p7zip-full
-# sudo apt-get -y install openjdk-7-jdk 
+sudo apt-get install oracle-java8-installer
+# sudo apt-get -y install openjdk-8-jdk 
 # sudo apt-get -y install nautilus-dropbox
 # sudo apt-get -y install idle3
