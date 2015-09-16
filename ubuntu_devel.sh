3265a856fcc7d05b65a85e28bf79a4ab955993fd
Xiangyu Bu <xybu92@live.com>
2015-09-16 00:33:32 -0400

    Add ack-grep package

--- ubuntu_devel.sh
+++ ubuntu_devel.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get -y install build-essential cmake automake gcc g++ gdb valgrind nemiver curl vim-gtk gawk ssh libcurl4-openssl-dev python-software-properties software-properties-common ack-grep
+sudo apt-get -y install ack-grep build-essential cmake automake gcc g++ gdb valgrind nemiver curl vim-gtk gawk ssh libcurl4-openssl-dev python-software-properties software-properties-common ack-grep
 sudo apt-get -y install p7zip-full
 sudo apt-get install oracle-java8-installer
 # sudo apt-get -y install openjdk-8-jdk 
