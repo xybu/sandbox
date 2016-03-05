32233543397c8f1434b0d10f746ff1d308c0b645
Xiangyu Bu <xybu92@live.com>
2016-03-04 21:45:19 -0500

    Update ubuntu_devel.sh

--- ubuntu_devel.sh
+++ ubuntu_devel.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get -y install ack-grep build-essential cmake automake gcc g++ gdb valgrind nemiver curl vim gawk ssh libcurl4-openssl-dev python-software-properties software-properties-common ack-grep
+sudo apt-get -y install ack-grep build-essential cmake automake gcc g++ gdb valgrind nemiver curl vim gawk ssh libcurl4-openssl-dev openssl python-software-properties software-properties-common ack-grep
 sudo apt-get -y install p7zip-full
-# sudo apt-get install oracle-java8-installer
-sudo apt-get -y install openjdk-8-jdk 
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get install oracle-java8-installer
+# sudo apt-get -y install openjdk-8-jdk 
 # sudo apt-get -y install nautilus-dropbox
 # sudo apt-get -y install idle3
 sudo apt-get -y install git mercurial
 alias vi=vim
+
+# Configure Mercurial
+echo "[ui]" > ~/.hgrc
+echo "username = Xiangyu Bu <xybu92@live.com>" >> ~/.hgrc
+echo "verbose = True" >> ~/.hgrc
+
+# Configure Git
+git config --global user.name "Xiangyu Bu"
+git config --global user.email "xybu92@live.com"
 git config --global core.editor vim
