0952ca97b410f84f7be4c9b3230cbd05c19b0c27
Xiangyu Bu <xybu92@live.com>
2016-03-04 21:49:46 -0500

    Update ubuntu_devel.sh

--- ubuntu_devel.sh
+++ ubuntu_devel.sh
@@ -4,6 +4,7 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt-get -y install ack-grep build-essential cmake automake gcc g++ gdb valgrind nemiver curl vim gawk ssh libcurl4-openssl-dev openssl python-software-properties software-properties-common ack-grep
 sudo apt-get -y install p7zip-full
 sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
 sudo apt-get install oracle-java8-installer
 # sudo apt-get -y install openjdk-8-jdk 
 # sudo apt-get -y install nautilus-dropbox
