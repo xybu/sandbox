132fbc6b1b10cd8f37a8f0a378ac962e634bbde1
Xiangyu Bu <xybu92@live.com>
2016-10-19 17:21:31 -0400

    Add more details to Logitech M570 script.

--- linux/logitech_m570_buttons.sh
+++ linux/logitech_m570_buttons.sh
@@ -4,8 +4,10 @@
 # PgUp / PgDn buttons according to
 # http://lovingboth.com/logitech-m570-trackball-buttons-in-linux/
 
+# Add required package.
 sudo apt install xbindkeys xautomation
 
+# Write the key binding.
 cat > ~/.xbindkeysrc << EOF
 "xte 'key Page_Up'"
 b:9
@@ -13,3 +15,10 @@ b:9
 "xte 'key Page_Down'"
 b:8
 EOF
+
+# Setup crontab so that it starts automatically.
+cron_line="@reboot xbindkeys"
+(crontab -l; echo "$cron_line" ) | crontab -
+
+# Start the program now.
+xbindkeys
