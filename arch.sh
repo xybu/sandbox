72dd8a04b52c5f6cbd78bb2e519666ba067841a6
Xiangyu Bu <xybu@users.noreply.github.com>
2017-03-13 19:26:24 -0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -5,4 +5,4 @@ sudo pacman -Syu
 sudo pacman -S intel-ucode
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo pacman -S ntp vim base-devel wget curl
+sudo pacman -S ntp vim base-devel wget curl ack cmake valgrind gawk mercurial
